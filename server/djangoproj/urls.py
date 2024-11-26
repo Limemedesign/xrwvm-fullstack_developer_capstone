6e373ed2f62abeba966bfe1f08b01f8413e3f7a2
limemedesign <limemedesign@gmail.com>
2024-11-25 22:43:57 -0500

    revised :all 1

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,20 +21,19 @@ from django.conf import settings
 from djangoapp import views  # Add this import to use views.get_cars
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-
+    path("admin/", admin.site.urls),
+    path("login/", TemplateView.as_view(template_name="index.html")),
+    path("register/", TemplateView.as_view(template_name="index.html")),
     # path('get_cars/', TemplateView.as_view(template_name="index.html")),
-    path('get_cars/', views.get_cars, name='getcars'),
-    
+    path("get_cars/", views.get_cars, name="getcars"),
     # dealers
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
+    path("dealers/", TemplateView.as_view(template_name="index.html")),
+    path("dealer/<int:dealer_id>", TemplateView.as_view(template_name="index.html")),
+    path(
+        "postreview/<int:dealer_id>", TemplateView.as_view(template_name="index.html")
+    ),
+    path("about/", TemplateView.as_view(template_name="About.html")),
+    path("contact/", TemplateView.as_view(template_name="Contact.html")),
+    path("djangoapp/", include("djangoapp.urls")),
+    path("", TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
