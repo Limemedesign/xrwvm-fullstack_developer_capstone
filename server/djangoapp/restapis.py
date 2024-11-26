4dc6c2995499461661023428eeb4ea69107fb689
limemedesign <limemedesign@gmail.com>
2024-11-25 22:40:47 -0500

    revised : populate -3, restapis

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,22 +5,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -32,12 +31,11 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 
-
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"/analyze/"+text
+    request_url = sentiment_analyzer_url + "/analyze/" + text
     print(request_url)
     try:
         # Call get method of requests library with URL and parameters
@@ -48,13 +46,12 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 
-
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
